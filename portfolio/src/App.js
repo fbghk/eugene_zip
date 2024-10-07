75470a50f1efdc92a9675572533946af556ab8b5
fbghk <im706374@gmail.com>
2024-10-07 17:58:00 +0900

    Fix | App.js
    div 태그에 이름 추가

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      fontSize: '2rem' 
+    }}>
+      홍길동
+    </div>
+  );
+}
+
+export default App;
